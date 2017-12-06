f06d1146ca27672d926116444182aa7796a0802d
Marcel Verkerk <mverkerk@godaddy.com>
2017-12-06 08:43:22 -0700

    Bug fix - bugs in getApiParams - client was deleting the default searchUrl if none was provided in getApiParams - the unchanged `log` key was returned in the response

--- src/perf-utils.js
+++ src/perf-utils.js
@@ -39,23 +39,22 @@ class PUtils {
 
   getApiParams({ sla, debug, esTrace, esCreate, days, perc, padding, searchUrl, log }) {
     // app_info,platform,browser,environment,team are required values
-    const data = this.params.api_params;
-    data.sla = sla || this.params.api_params.sla;
-    data.flags.debug = debug || this.params.api_params.flags.debug;
-    data.flags.esTrace = esTrace || this.params.api_params.flags.esTrace;
-    data.flags.esCreate = esCreate || this.params.api_params.flags.esCreate;
-    data.baseline.days = days || this.params.api_params.baseline.days;
-    data.baseline.perc = perc || this.params.api_params.baseline.perc;
-    data.baseline.padding = padding || this.params.api_params.baseline.padding;
-    data.baseline.searchUrl = searchUrl || '';
+    const updatedParams = this.params.api_params;
+    updatedParams.sla = sla || this.params.api_params.sla;
+    updatedParams.flags.debug = debug || this.params.api_params.flags.debug;
+    updatedParams.flags.esTrace = esTrace || this.params.api_params.flags.esTrace;
+    updatedParams.flags.esCreate = esCreate || this.params.api_params.flags.esCreate;
+    updatedParams.baseline.days = days || this.params.api_params.baseline.days;
+    updatedParams.baseline.perc = perc || this.params.api_params.baseline.perc;
+    updatedParams.baseline.padding = padding || this.params.api_params.baseline.padding;
 
-    if (!searchUrl)
-      delete data.baseline.searchUrl;
+    if (searchUrl)
+      updatedParams.baseline.searchUrl = searchUrl;
 
-    if (log && typeof log === 'object') {
-      this.params.api_params.log = Object.assign(this.params.api_params.log, log);
-    }
-    return data;
+    if (log && typeof log === 'object')
+      updatedParams.log = Object.assign(this.params.api_params.log, log);
+
+    return updatedParams;
   }
 
   getInjectJS(injectType, visualCompleteMark) {
